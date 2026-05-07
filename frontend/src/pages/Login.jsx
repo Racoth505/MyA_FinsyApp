b8686df17149ccaa3b5e6a61fbbfa5ebdda658ed
0bby1 <cesarava06@gmail.com>
2026-05-07 15:13:17 -0600

    2da prueba

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -57,7 +57,7 @@ export default function Login() {
   return (
     <main className="page">
       <section className="left">
-        <div className="logo">PRUEBA</div>
+        <div className="logo">FINSY</div>
       </section>
 
       <section className="right">
