37b6aabd8c8578920bc8bffeec9f1dcadeae4979
0bby1 <cesarava06@gmail.com>
2026-05-07 15:50:09 -0600

    Prubea

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -57,7 +57,7 @@ export default function Login() {
   return (
     <main className="page">
       <section className="left">
-        <div className="logo">PRUEBA</div>
+        <div className="logo">PRUEBA 10</div>
       </section>
 
       <section className="right">
