a880ca511cf0386e5a09acbe01f0eb4b9cc48bf6
Racoth505 <rafafl2001@gmail.com>
2026-05-07 08:44:52 -0600

    Listo para AWS

--- Backend/db.js
+++ Backend/db.js
@@ -1,10 +1,14 @@
 const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '12345678',
-  database: 'Finanzas'
+  // Si no existe la variable de entorno, usará 'localhost' por defecto (para desarrollo local)
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'contraseña',
+  database: process.env.DB_NAME || 'Finanzas',
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
 });
 
 module.exports = db;
